3e70c014b7961fd804026a537ab398ae74822437
Marcin Chrost <marcin.chrost@contractors.roche.com>
2021-04-20 22:42:49 +0200

    bare version for workshop

--- src/main/java/com/chrosciu/rxmastermindclient/runner/GameRunner.java
+++ src/main/java/com/chrosciu/rxmastermindclient/runner/GameRunner.java
@@ -1,31 +1,19 @@
 package com.chrosciu.rxmastermindclient.runner;
 
-import com.chrosciu.rxmastermindclient.service.GameService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
-import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
-import java.util.concurrent.CountDownLatch;
-
 @Component
-@Profile("!test")
 @RequiredArgsConstructor
 @Slf4j
 public class GameRunner implements CommandLineRunner {
-    private final GameService gameService;
 
     @Override
     public void run(String... args) throws Exception {
-        CountDownLatch countDownLatch = new CountDownLatch(1);
-        gameService.getResults()
-                .doFinally(signalType -> countDownLatch.countDown())
-                .subscribe(
-                        r -> System.out.println("Result: " + r),
-                        e -> log.warn("Error:", e)
-                );
-        countDownLatch.await();
+        log.info("IMPLEMENT ME !");
+        //TODO Implement game logic
     }
 
 }
--- src/test/java/com/chrosciu/rxmastermindclient/RxMastermindClientApplicationTests.java
+++ src/test/java/com/chrosciu/rxmastermindclient/RxMastermindClientApplicationTests.java
@@ -2,10 +2,8 @@ package com.chrosciu.rxmastermindclient;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
-@ActiveProfiles("test")
 class RxMastermindClientApplicationTests {
 
     @Test
