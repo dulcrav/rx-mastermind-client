cc2cce480713983c565fc942e560660ffbe4b17d
Marcin Chrost <marcin.chrost@contractors.roche.com>
2021-04-20 23:08:11 +0200

    added blockhound

--- src/main/java/com/chrosciu/rxmastermindclient/RxMastermindClientApplication.java
+++ src/main/java/com/chrosciu/rxmastermindclient/RxMastermindClientApplication.java
@@ -2,11 +2,13 @@ package com.chrosciu.rxmastermindclient;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import reactor.blockhound.BlockHound;
 
 @SpringBootApplication
 public class RxMastermindClientApplication {
 
     public static void main(String[] args) {
+        BlockHound.install();
         SpringApplication.run(RxMastermindClientApplication.class, args);
     }
 
