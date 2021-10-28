4fdc28110c20d5db437508b63ff8cfac3a13d41f
Marcin Chrost <marcinc@backbase.com>
2021-10-28 23:31:35 +0200

    Server url changed to property

--- src/main/java/com/chrosciu/rxmastermindclient/config/WebClientConfig.java
+++ src/main/java/com/chrosciu/rxmastermindclient/config/WebClientConfig.java
@@ -1,16 +1,14 @@
 package com.chrosciu.rxmastermindclient.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
 public class WebClientConfig {
-    private static final String SERVER_URL = "http://localhost:8080";
-
     @Bean
-    public WebClient webClient() {
-        return WebClient.builder().baseUrl(SERVER_URL).build();
+    public WebClient webClient(@Value("${mastermind.server.url}") String serverUrl) {
+        return WebClient.builder().baseUrl(serverUrl).build();
     }
-
 }
